bcf08adfc24f37434d8142a0aefb4c3ab5a30569
EltajGafarli <gafarlieltaj@gmail.com>
2023-12-18 19:25:54 +0400

    initial commit

---
+++ cards/src/main/java/com/example/cards/dto/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.cards.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ResponseDto {
+
+    private String statusCode;
+    private String statusMsg;
+}
---
+++ cards/src/main/java/com/example/cards/service/CardsService.java
@@ -0,0 +1,66 @@
+package com.example.cards.service;
+
+import com.example.cards.audit.CardsConstants;
+import com.example.cards.dto.CardsDto;
+import com.example.cards.entity.Cards;
+import com.example.cards.exception.CardAlreadyExistsException;
+import com.example.cards.exception.ResourceNotFoundException;
+import com.example.cards.mapper.CardsMapper;
+import com.example.cards.repository.CardsRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class CardsService {
+    private final CardsRepository cardsRepository;
+
+    @Transactional
+    public void createCard(String mobileNumber) {
+        Optional<Cards> optionalCards= cardsRepository.findByMobileNumber(mobileNumber);
+        if(optionalCards.isPresent()){
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber "+mobileNumber);
+        }
+        cardsRepository.save(createNewCard(mobileNumber));
+    }
+    private Cards createNewCard(String mobileNumber) {
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
+    }
+
+    public CardsDto fetchCard(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        return CardsMapper.mapToCardsDto(cards, new CardsDto());
+    }
+
+    @Transactional
+    public boolean updateCard(CardsDto cardsDto) {
+        Cards cards = cardsRepository.findByCardNumber(cardsDto.getCardNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "CardNumber", cardsDto.getCardNumber()));
+        CardsMapper.mapToCards(cardsDto, cards);
+        cardsRepository.save(cards);
+        return  true;
+    }
+
+    @Transactional
+    public boolean deleteCard(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        cardsRepository.deleteById(cards.getCardId());
+        return true;
+    }
+}
